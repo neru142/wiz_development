198ffc1b93ca3552f57767ab2dc83d3ec91e16df
nacho146 <neru142_git@ccmail.uk>
2022-11-09 11:12:56 +0900

    update

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
 app = Flask(__name__)
 
 # get environmental value from heroku
-ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
-CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
+ACCESS_TOKEN = os.environ["j8aO/SI10O5vX7pbpTTJ4tg+4iyFkbXTrYN3R2NUo/6Bz5cMPl/SFj4De8yxo6n2xwXAQ4VnsU3Yw0EXfNqyC/nUihg4beRKy97ISXPDh80dYRDoJTXeJeBezGrDAgaTiQ67HgtwTkVXRZlxlCoIywdB04t89/1O/w1cDnyilFU="]
+CHANNEL_SECRET = os.environ["22519454aad099d06272279709bbabbe"]
 line_bot_api = LineBotApi(ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
