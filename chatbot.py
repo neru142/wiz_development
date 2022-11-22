96d805110480514efe01be8af58140033bd00dac
ねる <108409910+neru142@users.noreply.github.com>
2022-11-22 14:59:31 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,5 @@
+# ライブラリをインポート
 from flask import Flask, request, abort
-
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -7,45 +7,41 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage
 )
 import os
-
+#Flaskを準備
 app = Flask(__name__)
-
-#環境変数取得
-ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
-CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
-
-line_bot_api = LineBotApi(ACCESS_TOKEN)
-handler = WebhookHandler(CHANNEL_SECRET)
-
+#環境変数からLINE Access Tokenを設定
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+#環境変数からLINE Channel Secretを設定
+LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+#LineBotApiのインスタンスを生成
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+#WebhookHandlerのインスタンスを生成
+webhook_handler = WebhookHandler(LINE_CHANNEL_SECRET)
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
+    # HTTPリクエストヘッダからX-Line-Signatureを取り出す
     signature = request.headers['X-Line-Signature']
-
-    # get request body as text
+    #テキストでpostされたデータを取得
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
-    # handle webhook body
+    # webhookのbodyを解析する
+　#この結果はadd関数で受け取る
+　#なお、Signatureが一致していない時はInvalidSignatureError例外が発生する
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
-
-
+# MessageEvent
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+　　#テキストでの返信を行う
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
-
-
+        TextSendMessage(text=event.message.text )
+    )
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
